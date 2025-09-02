b6254faeca429eb751b27070615632f3f9af9dcf
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-01 23:19:58 -0300

    fix: adicionados atributos faltantes na classe Pet

--- src/app/models/Pet.ts
+++ src/app/models/Pet.ts
@@ -2,6 +2,10 @@ export default class Pet {
   id: string;
   nome: string;
   especie: string;
+  sexo: string;
+  idade: string;
+  porte: string;
+  informacoes?: string | null;
   foto_url?: string | null;
   status: 'Disponível' | 'Adotado' | 'Em tratamento' | string;
 
@@ -9,12 +13,20 @@ export default class Pet {
     id: string,
     name: string,
     especie: string,
+    sexo: string,
+    idade: string,
+    porte: string,
+    informacoes: string | null,
     photoUrl: string | null,
     status: 'Disponível' | 'Adotado' | 'Em tratamento' | string
   ) {
     this.id = id;
     this.nome = name;
     this.especie = especie;
+    this.sexo = sexo;
+    this.idade = idade;
+    this.porte = porte;
+    this.informacoes = informacoes;
     this.foto_url = photoUrl;
     this.status = status;
   }
